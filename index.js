8da40cb8de11c9ff2d6cc4e91feb228d4581433c
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-10-28 11:58:59 +0200

    get all valid sections

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function isValidSection() {}
+function isValidSection(section) {
+  const dataId = section.getAttribute("data-id");
+  if (!dataId) return false;
 
-function isValidArticle() {}
+  if (!dataId.startsWith("92")) {
+    return false;
+  }
+
+  return section;
+}
+
+function isValidArticle(article) {}
 
 function isValidDiv() {}
 
 function isValidB() {}
 
-document.querySelectorAll("section").forEach((section) => {
-  console.log("Hello World");
+document.addEventListener("DOMContentLoaded", () => {
+  const nodes = Array.from(document.querySelectorAll("section"))
+    .map((section) => {
+      const validSection = isValidSection(section);
+      if (!validSection) return;
+
+      const articles = section.querySelectorAll("article");
+      if (articles.length === 0) return;
+    })
+    .filter(Boolean);
+
+  console.log(nodes);
 });
